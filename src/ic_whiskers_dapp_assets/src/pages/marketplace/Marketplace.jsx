06dd3ad2122df2a7aeaebce179a6e43f25c149d6
Jesse Keogh <jesse.keogh74@gmail.com>
2022-07-06 23:32:23 +0100

    setting up grids and btns

--- src/ic_whiskers_dapp_assets/src/pages/marketplace/Marketplace.jsx
+++ src/ic_whiskers_dapp_assets/src/pages/marketplace/Marketplace.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  SimpleGrid,
+  Box,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuOptionGroup,
+  MenuDivider,
+  MenuItemOption,
+  Button,
+} from "@chakra-ui/react";
+
+const address =
+  "a004f41ea1a46f5b7e9e9639fbed84e037d9ce66b75d392d2c1640bb7a559cda"; // this is badbot address
 
 const Marketplace = () => {
   return (
-    <div>Marketplace</div>
-  )
-}
+    <>
+      <FilteringOptions />
+      <SimpleGrid columns={{ base: 2, md: 2, lg: 4 }} gap={2}>
+        <Box bg="tomato" height="80px"></Box>
+        <Box bg="tomato" height="80px"></Box>
+        <Box bg="tomato" height="80px"></Box>
+        <Box bg="tomato" height="80px"></Box>
+        <Box bg="tomato" height="80px"></Box>
+      </SimpleGrid>
+    </>
+  );
+};
 
-export default Marketplace
+// put below in a seperate file
+const FilteringOptions = () => {
+  return <RarityFilter />;
+};
+
+const RarityFilter = () => {
+  const [currentOption, setCurrentOption] = useState("Common");
+
+  return (
+    <Menu>
+      <MenuButton as={Button} colorScheme="blue">
+        {currentOption}
+      </MenuButton>
+      <MenuList minWidth="240px">
+        <MenuOptionGroup defaultValue="Common" title="Rarity">
+          <MenuItemOption
+            value={"Common"}
+            onClick={() => setCurrentOption("Common")}
+          >
+            Common
+          </MenuItemOption>
+          <MenuItemOption
+            value={"Uncommon"}
+            onClick={() => setCurrentOption("Uncommon")}
+          >
+            Uncommon
+          </MenuItemOption>
+          <MenuItemOption
+            value={"Rare"}
+            onClick={() => setCurrentOption("Rare")}
+          >
+            Rare
+          </MenuItemOption>
+          <MenuItemOption
+            value={"Epic"}
+            onClick={() => setCurrentOption("Epic")}
+          >
+            Epic
+          </MenuItemOption>
+          <MenuItemOption
+            value={"Legendary"}
+            onClick={() => setCurrentOption("Legendary")}
+          >
+            Legendary
+          </MenuItemOption>
+          <MenuItemOption
+            value={"Artifact"}
+            onClick={() => setCurrentOption("Artifact")}
+          >
+            Artifact
+          </MenuItemOption>
+        </MenuOptionGroup>
+      </MenuList>
+    </Menu>
+  );
+};
+export default Marketplace;
